9e866385a9e8963c219a499c3c7a385fb928883b
Benjamin Altpeter <hi@bn.al>
2023-07-20 21:24:44 +0200

    String patterns
    These are mostly useless because browsers only validate them
    on submit, but oh well.

--- src/schema-renderer/string.tsx
+++ src/schema-renderer/string.tsx
@@ -1,9 +1,10 @@
 import { objectStore } from '../store';
 import type { SchemaTypeRendererProps } from './index';
 
-export const StringRenderer = ({ path, elementIds }: SchemaTypeRendererProps) => (
+export const StringRenderer = ({ path, elementIds, schema }: SchemaTypeRendererProps) => (
     <input
         type="text"
+        pattern={schema.pattern}
         class="form-control form-control-sm"
         id={elementIds.input}
         value={objectStore.useTracked.getForPath(path)}
