72266569a5ef3a094558309336af976be2374ef8
Benjamin Altpeter <hi@bn.al>
2023-08-04 22:19:23 +0200

    Very basic form API with submit function

--- src/Form.tsx
+++ src/Form.tsx
@@ -5,11 +5,18 @@ import { SchemaRenderer } from './schema-renderer';
 import { objectStore } from './store';
 import { emtpyDefaultForJsonSchema } from './util';
 
+export type FormData = { object: unknown; event: 'submitted' | 'changed' };
+export type FormDataWithOldData = FormData & { oldObject: unknown; event: 'changed' };
+export type FormApi = {
+    submit: () => void;
+};
 export type FormProps = {
     id?: string;
     schema: JSONSchema7Definition;
 
-    onChange?: (object: unknown, oldObject: unknown) => void;
+    onSubmit?: (formData: FormData) => void;
+    onChange?: (formData: FormDataWithOldData) => void;
+    formApiRef?: { current: FormApi | null };
 };
 
 export const Form = ({ schema, ...props }: FormProps) => {
@@ -17,11 +24,18 @@ export const Form = ({ schema, ...props }: FormProps) => {
         objectStore.set.object(emtpyDefaultForJsonSchema(schema) as Record<string, unknown>);
     }, [schema]);
 
-    const rootId = props.id || `formj-${hash(schema)}`;
+    const formApi: FormApi = {
+        submit: () => props.onSubmit?.({ object: objectStore.useStore.getState().object, event: 'submitted' }),
+    };
 
     if (props.onChange)
-        objectStore.useStore.subscribe((state, prevState) => props.onChange?.(state.object, prevState.object));
+        objectStore.useStore.subscribe((state, prevState) =>
+            props.onChange?.({ object: state.object, oldObject: prevState.object, event: 'changed' })
+        );
 
+    if (props.formApiRef) props.formApiRef.current = formApi;
+
+    const rootId = props.id || `formj-${hash(schema)}`;
     return (
         <form id={rootId}>
             <SchemaRenderer schema={schema} id={rootId} path="$" required={false} />
